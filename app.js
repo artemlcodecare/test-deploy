2dfa59fa45417f242b546d9a682fb8e71e20911b
Artem Loiko <artem.l@code-care.pro>
2020-11-10 16:36:45 +0200

    Update app

--- app.js
+++ app.js
@@ -1,41 +1,45 @@
-var port = process.env.PORT || 8000,
-    http = require('http'),
-    fs = require('fs'),
-    html = fs.readFileSync('index.html', 'utf-8');
+var port = process.env.PORT || 8080,
+  http = require('http'),
+  fs = require('fs'),
+  html = fs.readFileSync('index.html', 'utf-8');
 
+var envs = `ENVS: ${process.env.REDIS_URL}, ${process.env.AWS_REGION}`;
 
-var envs = `ENVS: ${process.env.REDIS_URL}, ${process.env.AWS_REGION}`
-
-var log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
+var log = function (entry) {
+  fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
 };
 
 var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
+  if (req.method === 'POST') {
+    var body = '';
 
-        req.on('data', function(chunk) {
-            body += chunk;
-        });
+    req.on('data', function (chunk) {
+      body += chunk;
+    });
 
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
+    req.on('end', function () {
+      if (req.url === '/') {
+        log('Received message: ' + body);
+      } else if ((req.url = '/scheduled')) {
+        log(
+          'Received task ' +
+            req.headers['x-aws-sqsd-taskname'] +
+            ' scheduled at ' +
+            req.headers['x-aws-sqsd-scheduled-at']
+        );
+      }
 
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html.replace('{{DATA}}', envs));
-        res.end();
-    }
+      res.writeHead(200, 'OK', { 'Content-Type': 'text/plain' });
+      res.end();
+    });
+  } else {
+    res.writeHead(200);
+    res.write(html.replace('{{DATA}}', envs));
+    res.end();
+  }
 });
 
-// Listen on port 3000, IP defaults to 127.0.0.1
+// Listen on port 8080, IP defaults to 127.0.0.1
 server.listen(port);
 
 // Put a friendly message on the terminal
